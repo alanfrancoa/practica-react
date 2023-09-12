89f66dfa615e0ad2b75cb0b4e854508bb3525ed6
alanfrancoa <alvarezalanfranco@gmail.com>
2023-09-12 17:30:30 -0300

    Creacion de componente, con codigo eslintado

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import './App.css';
+import { TwitterFollowCard } from './TwitterFollowCard.jsx';
 
-function App() {
-  const [count, setCount] = useState(0)
+const users = [
+    {
+        userName: 'midudev',
+        name: 'Miguel Angel Duran',
+        isFollowing: true
+    },
+    {
+        userName: 'alanfrancoa',
+        name: 'Alan Franco Alvarez',
+        isFollowing: false
+    },
+    {
+        userName: 'ladygaga',
+        name: 'Lady Garlic',
+        isFollowing: false
+    }
+    ,
+    {
+        userName: 'elonmusk',
+        name: 'Elon Musk',
+        isFollowing: true
+    }
+]
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export function App (){
+
+    return (
+        <div className="App">{
+            users.map(user => {
+                const {userName, name, isFollowing} = user
+                return(
+                    <TwitterFollowCard
+                        key={userName}
+                        userName={userName}
+                        initialIsFollowing={isFollowing}>
+                        {name}
+                    </TwitterFollowCard>
+                    
+                )
+            })
+        }        
+        </div>
+    )
 }
-
-export default App
--- projects/00-hola-mundo/src/main.jsx
+++ projects/00-hola-mundo/src/main.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { App } from './App.jsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+/*const Button = ({text}) => {// Los componentes se definen en PascalCase
+  return (<button>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5m6 4.125l2.25 2.25m0 0l2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
+    </svg>
+    {text}
+  </button>)
+}
+
+root.render(
+  <React.Fragment>
+    <Button text="hola, hola"/>
+    <Button text="hola, hola2"/>
+    <Button text="hola, hola3"/>
+  </React.Fragment>
+);
+
+*/
+root.render(
+<App />
+);
