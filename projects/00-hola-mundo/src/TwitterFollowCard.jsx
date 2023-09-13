ab587b13c4f30f6a4e24c4c319498b3894e8739d
alanfrancoa <alvarezalanfranco@gmail.com>
2023-09-13 12:39:47 -0300

    Se agrega verificacion de datos en tw-follow-card

--- projects/00-hola-mundo/src/TwitterFollowCard.jsx
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -1,34 +1,52 @@
-import { useState } from "react";
+import { useState } from 'react'
+import PropTypes from 'prop-types';
 
-export function TwitterFollowCard ({ userName, children, initialIsFollowing }){
-    const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
+export function TwitterFollowCard ({ children, userName, initialIsFollowing }) {
+  const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
 
-    const text = isFollowing ? 'Siguiendo' : 'Seguir';
-    const buttonClassName = isFollowing ? 'tw-followCard-button is-following' : 'tw-followCard-button';
-    const imageSrc = `https://unavatar.io/${userName}` ; //siempre con backtics!
+  console.log('[TwitterFollowCard] render with userName: ', userName)
 
-    const handleClick = () => { // estado interno
-       setIsFollowing(!isFollowing) 
-    }
+  const text = isFollowing ? 'Siguiendo' : 'Seguir'
+  const buttonClassName = isFollowing
+    ? 'tw-followCard-button is-following'
+    : 'tw-followCard-button'
 
-    return (
-            <article className="tw-followCard"> 
-            <header className="tw-followCard-header">
-                <img 
-                className="tw-followCard-avatar" 
-                alt="El Avatar de un random" 
-                src={imageSrc}/>
-                <div className="tw-followCard-info"> 
-                    <strong> {children} </strong>
-                    <span className="tw-followCard-infoUserName">@{userName} </span>
-                </div>
-            </header>
-            <aside>
-                <button className={buttonClassName} onClick={handleClick}>              
-                    <span className="tw-followCard-text"> {text} </span>
-                    <span className="tw-followCard-stopFollow">Dejar de seguir</span>
-                </button>
-            </aside>
-        </article>
-    )
+  const handleClick = () => {
+    setIsFollowing(!isFollowing)
+  }
+
+  return (
+    <article className='tw-followCard'>
+      <header className='tw-followCard-header'>
+        <img
+          className='tw-followCard-avatar'
+          alt='El avatar de midudev'
+          src={`https://unavatar.io/${userName}`}
+        />
+        <div className='tw-followCard-info'>
+          <strong>{children}</strong>
+          <span className='tw-followCard-infoUserName'>@{userName}</span>
+        </div>
+      </header>
+
+      <aside>
+        <button className={buttonClassName} onClick={handleClick}>
+          <span className='tw-followCard-text'>{text}</span>
+          <span className='tw-followCard-stopFollow'>Dejar de seguir</span>
+        </button>
+      </aside>
+    </article>
+  )
 }
+
+TwitterFollowCard.propTypes = {
+    children: PropTypes.node.isRequired,
+    userName: PropTypes.string.isRequired,
+    initialIsFollowing: PropTypes.bool.isRequired,
+} 
+/*tendrá la validación de propiedades necesaria, y el mensaje de error de ESLint debería desaparecer. Esto ayuda a garantizar que las propiedades pasadas al componente sean del tipo correcto y estén presentes cuando se llame al componente */
+  
+  
+  
+  
+  
