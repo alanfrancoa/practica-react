7fc12f4b429f69292707d91a8b89c229b31cc8d1
alanfrancoa <alvarezalanfranco@gmail.com>
2023-09-14 16:54:48 -0300

    Punto de entrada configurado y comienzo del App

---
+++ projects/05-prueba-tecnica/main.jsx
@@ -0,0 +1,4 @@
+import { createRoot } from 'react-dom/client'
+import { App } from './src/App'
+const root = createRoot(document.getElementById('app'))
+root.render(<App />)
---
+++ projects/05-prueba-tecnica/src/App.jsx
@@ -0,0 +1,5 @@
+export function App () {
+  return (
+    <h1>App de Gatuchis</h1>
+  )
+}
