a8f98299d19a4db10505abaeac181ca0e42d5bf7
alanfrancoa <alvarezalanfranco@gmail.com>
2023-09-14 19:42:28 -0300

    Objetivo del readme, cumplido, falta aplicar estilos.

---
+++ projects/05-prueba-tecnica/README.md
@@ -0,0 +1,5 @@
+APIs:
+-Facts Random: https://catfact.ninja/fact
+-Image Random: https://cataas.com/cat/says/hello
+
+Recupera un hecho aleatorio de gatos de la primer API y muestra una imagen de un gato con la primera palabra del hecho recuperado usando la segunda API.
--- projects/05-prueba-tecnica/src/App.jsx
+++ projects/05-prueba-tecnica/src/App.jsx
@@ -1,5 +1,33 @@
+import { useEffect, useState } from 'react'
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+const CAT_PREFIX_END_POINT = 'https://cataas.com/'
+// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstThreeWords}?size=50&color=red&json=true`
+
 export function App () {
+  const [fact, setFact] = useState()
+  const [imageUrl, setImageUrl] = useState()
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then(res => res.json())
+      .then(data => {
+        const { fact } = data
+        setFact(fact)
+        const firstThreeWords = fact.split(' ', 3).join(' ') // split(' ').slice(0, 3).join(' ') para que sean 3 palabras
+        console.log(firstThreeWords)
+
+        fetch(`https://cataas.com/cat/says/${firstThreeWords}?size=50&color=red&json=true`)
+          .then(res => res.json())
+          .then(response => {
+            const { url } = response
+            setImageUrl(url)
+          })
+      })
+  }, [])
   return (
-    <h1>App de Gatuchis</h1>
-  )
+    <main>
+      <h1>App de Gatuchis</h1>
+      {fact && <p>{fact}</p>}
+      {imageUrl && <img src={`${CAT_PREFIX_END_POINT}${imageUrl}`} alt={`Image extracted using the frist three words for ${fact}`} />}
+    </main>
+  ) // renderizado condicional
 }
